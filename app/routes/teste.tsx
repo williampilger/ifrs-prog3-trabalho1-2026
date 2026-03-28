92b9b53464a7b6aef12e23537c6427700246ccb4
edu.pilger.will <edu.pilger.will@gmail.com>
2026-03-27 21:07:34 -0300

    init

---
+++ app/routes/teste.tsx
@@ -0,0 +1,15 @@
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function TestePage() {
+  return <>
+    <span>Olá, Mundo!</span>
+    <span>Rota Teste!</span>
+  </>
+}
