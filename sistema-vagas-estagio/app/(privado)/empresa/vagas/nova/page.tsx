748e40866f16c5017ae00964cdc79336fa31f85b
Felipe <felipeledur03@hotmail.com>
2026-06-14 14:28:46 -0300

    [add] novaVaga

---
+++ sistema-vagas-estagio/app/(privado)/empresa/vagas/nova/page.tsx
@@ -0,0 +1,9 @@
+import Card from "@/app/components/Card";
+
+export default function NovaVaga() {
+    return (
+        <Card className="max-w-2xl mx-auto">
+            <h1>Nova Vaga</h1>
+        </Card>
+    );
+}
